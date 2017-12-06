94d7e1c4071fc824bb80f697d6479db4ec19d5ee
Stefan Paquay <stefanpaquay@gmail.com>
2017-12-06 15:21:35 -0500

    Added C interface for a lot of options.

--- rehuel.cpp
+++ rehuel.cpp
@@ -1,44 +1,135 @@
 #include "rehuel.h"
 
-// This C++ source file contains the C routines.
-/*
+#include "irk.hpp"
+#include "newton.hpp"
 
-rehuel_handle *rehuel_initialize()
+// This file contains implementation details of the C interface.
+
+struct rehuel_handle_guts {
+	irk::solver_coeffs  *irk_coeffs;
+	irk::solver_options *irk_opts;
+	newton::options     *newton_opts;
+
+};
+
+
+// Init and finalize:
+rehuel_handle rehuel_initialize()
 {
-	rehuel_handle *rh = new rehuel_handle;
-	rh->coeffs = new rehuel::solver_coeffs;
-	rh->options = new rehuel::solver_options;
+	rehuel_handle rh = new rehuel_handle_guts;
+	// The solver always needs a newton::options.
+	// The rest can be null until needed.
+	rh->irk_coeffs = nullptr;
+	rh->irk_opts = nullptr;
+	rh->newton_opts = new newton::options;
+
 
 	return rh;
 }
 
-
-void rehuel_finalize( rehuel_handle *rh )
+void rehuel_finalize( rehuel_handle rh )
 {
-	delete rh->coeffs;
-	delete rh->options;
+	if( rh->newton_opts ) delete rh->newton_opts;
+	if( rh->irk_coeffs  ) delete rh->irk_coeffs;
+	if( rh->irk_opts  )   delete rh->irk_opts;
 
 	delete rh;
 }
 
 
-void rehuel_set_solver_coefficients( rehuel_handle *rh, int solver )
+
+// Setings:
+// Newton:
+void rehuel_newton_set_tol( rehuel_handle rh, double tol )
 {
-	*rh->coeffs = rehuel::get_solver_coefficients( solver );
+	rh->newton_opts->tol = tol;
 }
 
-int rehuel_verify_solver_coefficients( rehuel_handle *rh )
+void rehuel_newton_set_maxit( rehuel_handle rh, int maxit )
 {
-	return rehuel::verify_solver_coeffs( *rh->coeffs );
+	rh->newton_opts->maxit = maxit;
 }
 
-int rehuel_integrate_ode( rehuel_handle *rh, double t0, double t1, int size,
-                          ode_rhs f, ode_jac J, int n_vals,
-                          double *t_vals, double **y_vals )
+void rehuel_newton_set_time_internals( rehuel_handle rh, int enable )
 {
-	// This is tricky...
-	int odeint_status = 0;
-
-	return odeint_status;
+	rh->newton_opts->time_internals = enable;
 }
-*/
+
+void rehuel_newton_set_max_step( rehuel_handle rh, double max_step )
+{
+	rh->newton_opts->max_step = max_step;
+}
+
+void rehuel_newton_set_refresh_jacobi_matrix( rehuel_handle rh, int refresh )
+{
+	rh->newton_opts->refresh_jac = refresh;
+}
+
+
+// IRK:
+void rehuel_irk_set_solver_coefficients( rehuel_handle rh, int method )
+{
+	if( !rh->irk_coeffs ){
+		rh->irk_coeffs = new irk::solver_coeffs;
+	}
+	*rh->irk_coeffs = irk::get_coefficients( method );
+}
+
+void rehuel_irk_set_internal_solver( rehuel_handle rh, int internal_solver )
+{
+	if( !rh->irk_opts ){
+		rh->irk_opts = new irk::solver_options;
+		rh->irk_opts->newton_opts = rh->newton_opts;
+		*rh->irk_opts = irk::default_solver_options();
+	}
+	rh->irk_opts->internal_solver = internal_solver;
+}
+
+void rehuel_irk_set_adaptive_time_step( rehuel_handle rh, int adaptive_dt )
+{
+	if( !rh->irk_opts ){
+		rh->irk_opts = new irk::solver_options;
+		rh->irk_opts->newton_opts = rh->newton_opts;
+		*rh->irk_opts = irk::default_solver_options();
+	}
+	rh->irk_opts->adaptive_step_size = adaptive_dt;
+}
+
+void rehuel_irk_set_relative_tolerance( rehuel_handle rh, double rel_tol )
+{
+	if( !rh->irk_opts ){
+		rh->irk_opts = new irk::solver_options;
+		rh->irk_opts->newton_opts = rh->newton_opts;
+		*rh->irk_opts = irk::default_solver_options();
+	}
+	rh->irk_opts->rel_tol = rel_tol;
+}
+
+void rehuel_irk_set_absolute_tolerance( rehuel_handle rh, double abs_tol )
+{
+	if( !rh->irk_opts ){
+		rh->irk_opts = new irk::solver_options;
+		rh->irk_opts->newton_opts = rh->newton_opts;
+		*rh->irk_opts = irk::default_solver_options();
+	}
+	rh->irk_opts->abs_tol = abs_tol;
+}
+
+void rehuel_irk_set_maximum_time_step( rehuel_handle rh, double max_dt )
+{
+	if( !rh->irk_opts ){
+		rh->irk_opts = new irk::solver_options;
+		rh->irk_opts->newton_opts = rh->newton_opts;
+		*rh->irk_opts = irk::default_solver_options();
+	}
+}
+
+void rehuel_irk_set_output_interval( rehuel_handle rh, int output_interval )
+{
+	if( !rh->irk_opts ){
+		rh->irk_opts = new irk::solver_options;
+		rh->irk_opts->newton_opts = rh->newton_opts;
+		*rh->irk_opts = irk::default_solver_options();
+	}
+	rh->irk_opts->out_int = output_interval;
+}
