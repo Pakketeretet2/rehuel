da0c7f3d2947d28686215107fa5e974bf08ea4c2
Stefan Paquay <stefanpaquay@gmail.com>
2018-01-22 14:13:56 -0500

    Updated script to check orders.

--- test_orders/check_orders.sh
+++ test_orders/check_orders.sh
@@ -12,9 +12,9 @@ do
 		rm $FILE;
 		touch $FILE;
 	fi
-	for dt in 2e-5 5e-5 1e-4 2e-4 5e-4 1e-3 2e-3 5e-3 1e-2 2e-2 5e-2 1e-1 2e-1 5e-1 1 2;
+	for dt in 1e-7 2e-7 5e-7 1e-6 2e-6 5e-6 1e-5 2e-5 5e-5 1e-4 2e-4 5e-4 1e-3 2e-3 5e-3 1e-2 2e-2 5e-2 1e-1 2e-1 5e-1 1 2 5 10 20 50;
 	do
-		../rehuel --t0 0.0 --t1 10.0 --dt $dt --method $M --test-exponential >> $FILE
+		../rehuel --t0 0.0 --t1 1000.0 --dt $dt --method $M --test-exponential >> $FILE
 	done
 	
 	mi=$(( mi + 1 ))
