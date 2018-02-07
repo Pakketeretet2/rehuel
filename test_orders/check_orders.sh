071960b322e3f2d828691c3a1ffe64a6daaa5bf9
Stefan Paquay <stefanpaquay@gmail.com>
2018-02-07 17:51:51 -0500

    commit before merge

--- test_orders/check_orders.sh
+++ test_orders/check_orders.sh
@@ -4,8 +4,7 @@
 #
 
 mi=1
-# for M in $( ../rehuel --print-all-methods )
-for M in FEHLBERG_54 CASH_KARP_54
+for M in $( ../rehuel --print-all-methods )
 do
 	FILE="err_"$M".dat"
 	if [ -f $FILE ]; then
@@ -14,7 +13,7 @@ do
 	fi
 	for dt in 2e-5 5e-5 1e-4 2e-4 5e-4 1e-3 2e-3 5e-3 1e-2 2e-2 5e-2 1e-1 2e-1 5e-1 1 2;
 	do
-		../rehuel --t0 0.0 --t1 10.0 --dt $dt --method $M --test-exponential >> $FILE
+		../rehuel --t0 0.0 --t1 1000.0 --dt $dt --method $M --test-exponential >> $FILE
 	done
 	
 	mi=$(( mi + 1 ))
