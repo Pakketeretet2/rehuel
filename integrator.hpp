a926964ad54bd029fbdc5c66028d4c1ff0d1dd05
Stefan Paquay <stefanpaquay@gmail.com>
2018-02-21 09:49:18 -0500

    Initial commit for integrator class.

---
+++ integrator.hpp
@@ -0,0 +1,20 @@
+#ifndef INTEGRATOR_HPP
+#define INTEGRATOR_HPP
+
+
+/**
+   \file integrator.hpp
+
+   \brief This file contains a class that implement a generic time
+   integration loop with a "plug-n-play" way to add different integrators.
+*/
+
+class integrator
+{
+
+
+};
+
+
+
+#endif // INTEGRATOR_HPP
