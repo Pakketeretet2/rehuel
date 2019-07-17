23e4d6ee91fa49c1f2a1e2b761105465f62a1b90
Stefan Paquay <stefanpaquay@gmail.com>
2019-07-17 08:02:39 -0400

    Made new code actually compile.

--- erk.hpp
+++ erk.hpp
@@ -453,9 +453,6 @@ template <typename functor_type> inline
 rk_output odeint( functor_type &func, double t0, double t1, const vec_type &y0)
 {
 	solver_coeffs sc = default_solver_options();
-	newton::options n_opts;
-	n_opts.tol = 0.1*std::min(sc.abs_tol, sc.rel_tol);
-	
 	return odeint(func, t0, t1, y0, s_opts);
 }
 
