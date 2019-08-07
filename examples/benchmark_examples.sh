a1dc10a61fe4e3f88c924437a9a109cc95070e39
Stefan Paquay <stefanpaquay@gmail.com>
2019-08-07 13:40:19 -0400

    Made some modifications to benchmark.

--- examples/benchmark_examples.sh
+++ examples/benchmark_examples.sh
@@ -6,7 +6,7 @@
 OUTPUT_DIR=output
 mkdir -p $OUTPUT_DIR
 
-METHODS=(RADAU_IIA_53 LOBATTO_IIIC_85 DORMAND_PRINCE_54 BOGACKI_SHAMPINE_32)
+METHODS=(RADAU_IIA_53 RADAU_IIA_95 LOBATTO_IIIC_43 LOBATTO_IIIC_85 DORMAND_PRINCE_54 FEHLBERG_54 BOGACKI_SHAMPINE_32)
 
 for METHOD in $METHODS
 do
@@ -15,17 +15,20 @@ do
 done
 
 
-for METHOD in $METHODS
+for METHOD in DORMAND_PRINCE_54 FEHLBERG_54 BOGACKI_SHAMPINE_32
 do
 	OUTPUT=$OUTPUT_DIR"/three_body_"$METHOD".dat"
 	./rehuel_example --method $METHOD --equation three-body > $OUTPUT
 done
 
-for METHODS in $METHODS
+for METHOD in $METHODS
 do
 	OUTPUT=$OUTPUT_DIR"/van_der_pol_"$METHOD".dat"
 	./rehuel_example --method $METHOD --equation van-der-pol > $OUTPUT
 done
 
-
-
+for METHOD in RADAU_IIA_53 RADAU_IIA_95 RADAU_IIA_137 LOBATTO_IIIC_43 LOBATTO_IIIC_85
+do
+	OUTPUT=$OUTPUT_DIR"/robertson_"$METHOD".dat"
+	./rehuel_example --method $METHOD --equation robertson --time-span 0 1e12 > $OUTPUT
+done
